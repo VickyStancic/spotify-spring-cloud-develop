945317c10b62ed4dd4693663eb1d67bede485a24
VickyStancic <victoriastancic@hotmail.com>
2022-11-07 20:48:48 -0300

    eureka

--- spotify-spring-cloud-develop/api-music/src/main/java/com/spotify/music/ApiMusicApplication.java
+++ spotify-spring-cloud-develop/api-music/src/main/java/com/spotify/music/ApiMusicApplication.java
@@ -2,10 +2,13 @@ package com.spotify.music;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableEurekaClient
+
 public class ApiMusicApplication {
 
     public static void main(String[] args) {
